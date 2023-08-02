88906e7d5f97d8e5d3cff3aba07644b0328ee0f7
Sander1994 <74837620+Sander1994@users.noreply.github.com>
2023-08-02 19:07:26 +0200

    Versuche Env zu benutzen (noch verbugged)

--- GanzSchoenCleverEnv.py
+++ GanzSchoenCleverEnv.py
@@ -1,6 +1,7 @@
 import gymnasium as gym
 from gymnasium import spaces
 import numpy as np
+import random
 
 
 class GanzSchonCleverEnv(gym.Env):
@@ -8,22 +9,17 @@ class GanzSchonCleverEnv(gym.Env):
 
     def __init__(self, rounds=10):
         super(GanzSchonCleverEnv, self).__init__()
-
         self.initial_rounds = rounds
         self.rounds = rounds
-        self.yellow_field = np.array([[3, 6, 5, 0], [2, 1, 0, 5], [1, 0, 2, 4], [0, 3, 4, 6]])
+        self.yellow_field = [[3, 6, 5, 0], [2, 1, 0, 5], [1, 0, 2, 4], [0, 3, 4, 6]]
         self.dice = self.roll_dice()
         self.rewards = {'row': [10, 10, 10, 25], 'col': [10, 14, 16, 20]}
         self.reward_flags = {'row': [False, False, False, False], 'col': [False, False, False, False]}
         self.extra_pick = False
         self.score = 0
-        self.state = {'field': self.yellow_field.copy(), 'dice': self.dice}
 
         self.action_space = spaces.MultiDiscrete([4, 4, 2])  # 4 rows, 4 columns, and binary flag for extra pick
-        self.observation_space = spaces.Dict({
-            'field': spaces.Box(low=0, high=6, shape=(4, 4), dtype=np.int32),
-            'dice': spaces.Box(low=1, high=6, shape=(2,), dtype=np.int32)
-        })
+        self.observation_space = spaces.Box(low=0, high=6, shape=(4 * 4 + 2,), dtype=np.int32)
 
     def step(self, action):
         row, col, extra_pick_action = action
@@ -70,29 +66,33 @@ class GanzSchonCleverEnv(gym.Env):
         if self.rounds == 0:
             terminated = True
 
-        self.state = {self.yellow_field.copy(), [2, 2]}
-        return np.array(self.state, dtype=np.float32), reward, terminated, truncated, info
+        return self._get_obs(), reward, terminated, truncated, info
 
     def reset(self, seed=None, options=None):
         super().reset(seed=seed)
 
-        self.yellow_field = np.array([[3, 6, 5, 0], [2, 1, 0, 5], [1, 0, 2, 4], [0, 3, 4, 6]])
+        self.yellow_field = [[3, 6, 5, 0], [2, 1, 0, 5], [1, 0, 2, 4], [0, 3, 4, 6]]
         self.reward_flags = {'row': [False, False, False, False], 'col': [False, False, False, False]}
         self.extra_pick = False
         self.score = 0
         self.rounds = self.initial_rounds
         self.dice = self.roll_dice()
-        self.state = {self.yellow_field.copy(), [2, 2]}
 
-        return np.array(self.state, dtype=np.float32), {}
+        return self._get_obs(), {}
 
     def render(self, mode='human'):
-        print(f'Yellow Field: {self.yellow_field}')
-        print(f'Dice: {self.dice}')
+        if mode == 'human':
+            print(f'Yellow Field: {self.yellow_field}')
+            print(f'Dice: {self.dice}')
+            print(f'Score: {self.score}')
+        elif mode == 'rgb_array':
+            raise NotImplementedError('rgb_array mode is not supported')
+        else:
+            raise ValueError(f'Render mode {mode} is not supported')
 
     @staticmethod
     def roll_dice():
-        return np.array([np.random.randint(1, 7), np.random.randint(1, 7)])
+        return random.randint(1, 6), random.randint(1, 6)
 
     def check_rewards(self):
         reward = 0
@@ -106,3 +106,16 @@ class GanzSchonCleverEnv(gym.Env):
         if all(self.yellow_field[i][i] == 0 for i in range(4)) and not self.extra_pick:
             self.extra_pick = True
         return reward
+
+    def _get_obs(self):
+        yellow_field_array = np.array(self.yellow_field, dtype=np.int32).flatten()
+        dice_array = np.array(list(self.dice), dtype=np.int32)
+
+        print(f'Yellow Field Array Shape: {yellow_field_array.shape}')
+        print(f'Dice Array Shape: {dice_array.shape}')
+
+        obs = np.concatenate((yellow_field_array, dice_array), axis=None)
+
+        print(f'Observation Shape: {obs.shape}')
+        return obs
+
--- main.py
+++ main.py
@@ -1,12 +1,37 @@
-import gymnasium as gym
-
 from stable_baselines3 import PPO
-from stable_baselines3.common.env_util import make_vec_env
-from stable_baselines3.common.env_checker import check_env
 from GanzSchoenCleverEnv import GanzSchonCleverEnv
 
-# Instantiate your environment
-env = GanzSchonCleverEnv(rounds=10)
 
-# Use the function to check your environment
-check_env(env)
+def train_and_test_model():
+    env = GanzSchonCleverEnv()  # Here we directly create one instance of the environment
+
+    model = PPO("MlpPolicy", env, verbose=1)  # We pass the single environment instance to the model
+    model.learn(total_timesteps=250)
+    model.save("ppo_ganzschoenclever")
+
+    del model
+
+    model = PPO.load("ppo_ganzschoenclever")
+
+    obs = env.reset()  # Use the single environment instance to reset and get the initial observation
+    while True:
+        try:
+            action, _states = model.predict(obs)
+        except ValueError as e:
+            print("Observation that caused error:", obs)
+            raise e
+        obs, rewards, terminated, truncated, info = env.step(action)
+        env.render("human")  # Use the single environment instance to render
+
+
+def main():
+    # Other parts of your code
+
+    # At some point you decide to start the training and testing of the model
+    train_and_test_model()
+
+    # More of your code
+
+
+if __name__ == "__main__":
+    main()
