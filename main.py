da49b4fe116f43d5b4d59249f509b922c228b12c
Sander1994 <74837620+Sander1994@users.noreply.github.com>
2023-08-22 18:12:16 +0200

    Version 1.4.3

--- main.py
+++ main.py
@@ -21,8 +21,8 @@ def train_and_test_model():
     fails = np.zeros(n_envs)
     fails_history = [[] for _ in range(n_envs)]
 
-    policy_kwargs = dict(net_arch=[256, 256, 256], activation_fn=nn.LeakyReLU)
-    model = PPO("MlpPolicy", env, gamma=0.95, learning_rate=0.0003*4, policy_kwargs=policy_kwargs,
+    policy_kwargs = dict(net_arch=[256, 256, 256], activation_fn=nn.Tanh)
+    model = PPO("MlpPolicy", env, gamma=0, learning_rate=0.0003*4, policy_kwargs=policy_kwargs,
                 ent_coef=0.01, clip_range=0.3, verbose=1, n_steps=int(2048/32), n_epochs=11,
                 batch_size=int(2048/8))
 
