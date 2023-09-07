d327674d7a7317034180d5b3b658ffca672a88c4
Sander1994 <74837620+Sander1994@users.noreply.github.com>
2023-09-07 19:00:23 +0200

    Version 2.3.4

--- main.py
+++ main.py
@@ -2,8 +2,8 @@ from Modell import *
 
 
 def main():
-    model_learn(net_arch=[512, 512, 512, 512], total_timesteps=1000000)
-    model_predict(n_envs=2)
+    # model_learn(net_arch=[512, 512, 512, 512], total_timesteps=1000000)
+    model_predict(n_envs=2, n_steps=22222)
 
 
 if __name__ == "__main__":
