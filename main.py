127a972a44734b0ca08d2c0615894d9a50bd724a
Sander1994 <74837620+Sander1994@users.noreply.github.com>
2023-09-08 14:27:54 +0200

    Version 2.3.10 - blaue Belohnungen für die unteren zwei Reihen x10 genommen um zu testen ob er dann versucht diese auszufüllen (bisher nicht geschehen)

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from Modell import *
 
 def main():
     # model_learn(net_arch=[512, 512, 512, 512], total_timesteps=1000000)
-    model_predict(n_envs=1, render=True, n_steps=5000)
+    model_predict(n_envs=1, render=True, n_steps=400)
 
 
 if __name__ == "__main__":
