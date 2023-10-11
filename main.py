2e91652b7ebf9953f5178ecfd6af732aef61275f
Sander1994 <74837620+Sander1994@users.noreply.github.com>
2023-10-11 14:36:10 +0200

    Version 4.0.0 - Environment completed

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from Modell import *
 
 def main():
     # model_learn(net_arch=[512, 512, 512, 512], total_timesteps=300000, ent_coef=0.1)
-    model_predict(n_envs=1, render=True, n_steps=4000)
+    model_predict(n_envs=1, render=True, n_steps=40000)
 
 
 if __name__ == "__main__":
